ef1f51a74c4a11da21111845c14d873d4d6d5e9d
lyncredulous <caitlyn.king2014@gmail.com>
2022-01-02 13:18:41 +0000

    Add reset grid logic

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function main() {
   const grid = document.querySelector(".grid");
+  const reset = document.querySelector(".btn");
   createGrid(grid, 16, 16);
   isHovered();
+  reset.addEventListener("click", (e) => resetGrid(grid));
 }
 
 function createGrid(grid, rows, columns) {
+  grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       const gridSquare = document.createElement("div");
@@ -19,6 +23,27 @@ function isHovered() {
   gridSquares.forEach((square) =>
     square.addEventListener("mouseover", (e) => square.classList.add("hovered"))
   );
+}
+
+function resetGrid(grid) {
+  let rows = parseInt(
+    prompt(
+      "Please enter the new amount of squares you want per row and column (max 100)."
+    )
+  );
+
+  if (isNaN(rows) || rows > 100) {
+    return resetGrid(grid);
+  }
+
+  let columns = rows;
+
+  while (grid.hasChildNodes()) {
+    grid.removeChild(grid.lastChild);
+  }
+
+  createGrid(grid, rows, columns);
+  isHovered();
 }
 
 main();
