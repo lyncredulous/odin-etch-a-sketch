f5edba5aac3ca3af8bfd1fa3e0a48525b03b7e29
lyncredulous <caitlyn.king2014@gmail.com>
2022-01-02 12:45:00 +0000

    Generate grid and hovered state

---
+++ script.js
@@ -0,0 +1,24 @@
+function main() {
+  const grid = document.querySelector(".container");
+  createGrid(grid, 16, 16);
+  isHovered();
+}
+
+function createGrid(grid, rows, columns) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.classList.add("square");
+      grid.append(gridSquare);
+    }
+  }
+}
+
+function isHovered() {
+  const gridSquares = Array.from(document.querySelectorAll(".square"));
+  gridSquares.forEach((square) =>
+    square.addEventListener("mouseover", (e) => square.classList.add("hovered"))
+  );
+}
+
+main();
